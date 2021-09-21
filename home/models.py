b3946badf91c83af1191e46e5880850433545b42
Crowdbotics <team@crowdbotics.com>
2021-09-21 20:02:26 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Home(models.Model):
+    "Generated Model"
+    name = models.BooleanField()
+    desc = models.CharField(
+        max_length=256,
+    )
